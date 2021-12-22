c54151a64ef0413f0aae98ba1ba92e0701c926f7
Igor Morozov <24278723+morozoffnor@users.noreply.github.com>
2021-12-22 19:36:44 +0300

    Read .yml file or create one if there is none

--- SkyblockStats/src/main/java/ml/igoryan/skyblockstats/Main.java
+++ SkyblockStats/src/main/java/ml/igoryan/skyblockstats/Main.java
@@ -1,23 +1,35 @@
 package ml.igoryan.skyblockstats;
 
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import ml.igoryan.skyblockstats.Managers.PropertiesManager;
 
-import java.util.Properties;
+import java.io.File;
 
 public final class Main extends JavaPlugin {
+
+    private File configFile = new File(getDataFolder(), "config.yml");
+    private FileConfiguration config = YamlConfiguration.loadConfiguration(configFile);
+
+    private File messagesFile = new File(getDataFolder(), "messages.yml");
 
     @Override
     public void onEnable() {
         // Plugin startup logic
-    PropertiesManager props = new PropertiesManager();
-    Properties config = props.getConfig();
-    System.out.println(config.entrySet());
+
+        // create config and messages file if there is none
+        if(!configFile.exists()) {
+            saveResource("config.yml", false);
+        }
+        if(!messagesFile.exists()) {
+            saveResource("messages.yml", false);
+        }
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
     }
+
 }
