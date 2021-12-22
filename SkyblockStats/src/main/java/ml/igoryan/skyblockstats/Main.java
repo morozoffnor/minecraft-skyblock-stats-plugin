f57b19937b0fd7669bd94bbf3ace5871587727cd
Igor Morozov <24278723+morozoffnor@users.noreply.github.com>
2021-12-22 05:12:08 +0300

    reading .properties file and initializing config at startup

--- SkyblockStats/src/main/java/ml/igoryan/skyblockstats/Main.java
+++ SkyblockStats/src/main/java/ml/igoryan/skyblockstats/Main.java
@@ -2,12 +2,18 @@ package ml.igoryan.skyblockstats;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import ml.igoryan.skyblockstats.Managers.PropertiesManager;
+
+import java.util.Properties;
+
 public final class Main extends JavaPlugin {
 
     @Override
     public void onEnable() {
         // Plugin startup logic
-
+    PropertiesManager props = new PropertiesManager();
+    Properties config = props.getConfig();
+    System.out.println(config.entrySet());
     }
 
     @Override
